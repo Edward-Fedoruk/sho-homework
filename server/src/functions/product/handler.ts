87293c18829a68b5e67cbd38a5cd7735058aed42
Fedoruk Eduard <eduard.fedoruk@nike.com>
2023-02-27 13:36:00 +0200

    add  find logic

--- server/src/functions/product/handler.ts
+++ server/src/functions/product/handler.ts
@@ -1,14 +1,24 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
+import { mockProductData } from './mockProducts';
 
-const getProductsById: ValidatedEventAPIGatewayProxyEvent<null> = async () => {
-  return formatJSONResponse({
-    id: "1",
-    title: "Product 1",
-    description: "Description for Product 1",
-    price: 10,
-    count: 1,
-  });
+
+const getProductsById: ValidatedEventAPIGatewayProxyEvent<null> = async (event) => {
+  const { productId } = event.pathParameters as Record<string, string | undefined>;
+  
+  const mockProduct = mockProductData.find((p) => p.id === productId)
+
+  if (!mockProduct) {
+    return formatJSONResponse({
+      code: 404,
+      body: {
+        error: `can not find product with id ${productId}`
+      }
+    })
+  }
+
+  return formatJSONResponse(mockProduct);
+  
 };
 
 export const main = getProductsById;
